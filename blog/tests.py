8ea095bfd4a6ac87d63d3e5b55ec14b125780173
Paola Sanchez <paolasc2652@gmail.com>
2022-08-06 14:58:44 -0400

    pip8 cleanup

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from . models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(
+            title='chocolate', author='vanilla', slug='caramel')
+
+    def test_post_model(self):
+        muffin = self.blog
+        # Checking if 'muffin' is an
+        # instance of 'Post' dabatase
+        self.assertTrue(isinstance(muffin, Post))
+        # Checking the string returned by a Post
+        # instance, a title called 'chocolate''
+        self.assertEqual(str(muffin), 'chocolate')
